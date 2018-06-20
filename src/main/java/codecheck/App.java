5362e962c11761795c6508cf52019356caeee614
code-project <dev@code-check.io>
2018-06-20 15:36:44 +0000

    Add new CLI related files

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -2,20 +2,9 @@ package codecheck;
 
 public class App {
 	public static void main(String[] args) {
-        int[] twice = new twice();
+		for (int i = 0, l = args.length; i < l; i++) {
+			String output = String.format("argv[%s]: %s", i, args[i]);
+			System.out.println(output);
+		}
 	}
-	
-	public static void twice(int[] array){
-		for (int element:array) {
-			if (element % 3 == 0 || Integer.toString(element).contains("3")) {
-             System.out.println("dumb");
-          } else if (element % 3 == 0) {
-            System.out.println("idiot");
-          } else if (Integer.toString(element).contains("3")){
-            System.out.println("stupid");
-          } else{
-          	System.out.println("smart");
-        }
-   }
-  }
 }
