13d66aa40238d096bdbe3a0c907ced9410bb390e
tie303223 <tie303223@S1307C00780-T1.tisnt.tis.co.jp>
2018-06-21 03:01:24 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -7,7 +7,7 @@ public class App {
 		//Scanner scan = new Scanner(System.in);
 
         // キーボードから整数の入力を受け取る
-        int score = args;
+        int score = Integer.parselnt(args);
 
         //scan.close();
         if (0 <= score || score <= 1000) {
