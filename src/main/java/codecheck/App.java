e6db1f33ac99adf7a3eee80ba571cba43a82c475
tie303223 <tie303223@S1307C00780-T1.tisnt.tis.co.jp>
2018-06-21 04:11:37 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,36 +4,28 @@ package codecheck;
 public class App {
 	public static void main(String[] args) {
 
-		//Scanner scan = new Scanner(System.in);
-
-        // キーボードから整数の入力を受け取る
 		int[] ia = new int[args.length];
-        //int score = Integer.parseInt(args);
 
 		for (int i = 0; i < args.length; i++) {
 			try {
-			ia[i] = Integer.parseInt(args[i]);
+				ia[i] = Integer.parseInt(args[i]);
 
-
-        if (0 <= ia[i] && ia[i] <= 1000) {
-		if (ia[i] % 3 == 0 && Integer.toString(ia[i]).contains("3")) {
-				System.out.println("dumb");
-
-			} else if (ia[i] % 3 == 0) {
-				System.out.println("idiot");
-			} else if (Integer.toString(ia[i]).contains("3")) {
-				System.out.println("stupidt");
-
-			} else {
-				System.out.println("smart");
+					if (0 <= ia[i] && ia[i] <= 1000) {
+						if (ia[i] % 3 == 0 && Integer.toString(ia[i]).contains("3")) {
+							System.out.println("dumb");
+						} else if (ia[i] % 3 == 0) {
+							System.out.println("idiot");
+						} else if (Integer.toString(ia[i]).contains("3")) {
+							System.out.println("stupidt");
+						} else {
+							System.out.println("smart");
+						}
+					} else {
+						System.out.println("invalid");
+					}
+				} catch (NumberFormatException e) {
+					System.out.println("invalid");
 			}
-        } else {
-        	System.out.println("invalid");
-        }
-
-			}catch (NumberFormatException e) {
-				System.out.println("invalid");
-		    }
-		}
 		}
 	}
+}
