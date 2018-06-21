50281e89d01b92ea8b4b3d87655594166cac0f01
tie303223 <tie303223@S1307C00780-T1.tisnt.tis.co.jp>
2018-06-21 21:48:04 +0900

    コミット

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,14 +5,14 @@ public class App {
 	public static void main(String[] args) {
 
 		int[] ia = new int[args.length];
-
+		String [] ib = new String[args.length];
 		for (int i = 0; i < args.length; i++) {
 			try {
 				ia[i] = Integer.parseInt(args[i]);
-
+				ib[i] = args[i];
 				byte[] bytes = Integer.toString(ia[i]).getBytes();
 
-				if  (Integer.toString(ia[i]).length() != bytes.length) {
+				if  (ib[i]).length() != bytes.length) {
 					if (0 <= ia[i] && ia[i] <= 1000) {
 						if (ia[i] % 3 == 0 && Integer.toString(ia[i]).contains("3")) {
 							System.out.println("dumb");
